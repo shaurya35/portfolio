895918bdcbbe7a5de39c7b87538c1d26a541a864
Shaurya <shauryajha35@gmail.com>
2026-07-16 23:22:00 +0530

    feat(web): compose homepage from hero/experience/blog sections

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,13 @@
+import { Hero } from "@/components/sections/hero";
+import { Experience } from "@/components/sections/experience";
+import { Blog } from "@/components/sections/blog";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Experience />
+      <Blog />
+    </>
+  );
+}
