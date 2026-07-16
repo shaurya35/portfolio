61c2fbd8c8613f7fcd66b88af7369d807ae2b51e
Shaurya <shauryajha35@gmail.com>
2026-07-17 01:23:06 +0530

    feat(web): add arrow-up-right icon

--- apps/web/components/icons.tsx
+++ apps/web/components/icons.tsx
@@ -61,6 +61,14 @@ export function ArrowRightIcon(props: IconProps) {
   );
 }
 
+export function ArrowUpRightIcon(props: IconProps) {
+  return (
+    <svg viewBox="0 0 256 256" fill="currentColor" {...props}>
+      <path d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z" />
+    </svg>
+  );
+}
+
 export function MediumIcon(props: IconProps) {
   return (
     <svg viewBox="0 0 256 256" fill="currentColor" {...props}>
